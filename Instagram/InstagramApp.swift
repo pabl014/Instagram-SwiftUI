59e8ac1c3757af3fc6db62c53c9fc158526b8875
Pawel Rudnik <pawelrudnik3@gmail.com>
2024-01-11 12:38:40 +0100

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Paweł Rudnik on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
